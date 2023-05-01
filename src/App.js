1a17375aea26e7bd4382035975b3b1a3f75a1e66
Anupam Prakash <anupamprakash82@gmail.com>
2023-05-01 12:13:37 +0530

    feature: loader component created
    created the loader component which will render in place of facts list until the data arrives from the database

--- src/App.js
+++ src/App.js
@@ -67,11 +67,21 @@ function App() {
   // 1. define state variable
   const [showForm, setShowForm] = useState(false);
   const [facts, setFacts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(function () {
     async function getFacts() {
-      const { data: facts, error } = await supabase.from("facts").select("*");
-      setFacts(facts);
+      setIsLoading(true);
+      const { data: facts, error } = await supabase
+        .from("facts")
+        .select("*")
+        .order("votesInteresting", { ascending: true })
+        .limit(199);
+
+      // here we are handling the case where we incoming data has some error or if we are not able to load the data
+      if (!error) setFacts(facts);
+      else alert("There was a problem getting data");
+      setIsLoading(false);
     }
     getFacts();
   }, []);
@@ -87,10 +97,14 @@ function App() {
 
       <main className="main">
         <CategoryFilter />
-        <FactList facts={facts} />
+        {isLoading ? <Loader /> : <FactList facts={facts} />}
       </main>
     </>
   );
+}
+
+function Loader() {
+  return <p className="message">Loading...</p>;
 }
 
 function Header({ showForm, setShowForm }) {
